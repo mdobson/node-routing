e6555ecd40e754335b0213f324c5016a1c7a0a4d
Matt Dobson <mdobson4@gmail.com>
2012-07-14 16:56:57 -0400

    Made routing function their own module.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var connect = require('connect')
-  , http = require('http');
+  , http = require('http')
+  , routing = require('./routing.js');
 
 var testRoute = function(){
   return 'test route';
@@ -9,50 +10,6 @@ var testRouteWithData = function(data){
   return JSON.stringify(data);
 }
 
-var RemoveSpaces = function(array){
-  for(var i = 0; i < array.length; i++){
-    if(array[i] == ""){
-      array.splice(i, 1);
-    }
-  }
-  return array;
-}
-
-var DetectRoute = function(routes, url){
-    console.log("Detecting Route for URL: " + url);
-    for(var key in routes){
-      routeParams = key.split('/');
-      routeParams = RemoveSpaces(routeParams);
-      for( var i = 0; i < routeParams.length; i++){
-        if(url.indexOf(routeParams[i]) != -1){
-          urlParams = url.split('/');
-	  urlParams = RemoveSpaces(urlParams);
-          if(urlParams.length == routeParams.length){
-            return { route : key, data : url };
-          }
-        }
-      }        
-    }
-
-}
-
-var RetrieveData = function(route, url){
-  if(route.indexOf(':') != -1){
-    data = new Object();
-    routePieces = route.split('/');
-    dataPieces = url.split('/');
-    for( var i=0; i < routePieces.length; i++){
-      if(routePieces[i].indexOf(':')  != -1){
-        data[routePieces[i].replace(/:/g, "")] = dataPieces[i];
-      }
-    }
-    return data;
-  }
-  else{
-    return;
-  }
-}
-
 var routes = {
   '/test/':testRoute,
   '/test/:id':testRouteWithData
@@ -60,28 +17,7 @@ var routes = {
 
 var app = connect()
   .use(function(req, res){
-  try{
-    if(req.url == '/favicon.ico'){
-      return;
-    }
-    var object = DetectRoute(routes, req.url);
-    console.log("Matched route results: " + JSON.stringify(object));
-    var data = RetrieveData(object.route, object.data);
-    console.log("Route data pulled: " + JSON.stringify(data));
-    if(data != undefined){
-      console.log("firing data route");
-      var ret = routes[object.route](data);
-      res.end(ret);
-    }
-    else{
-      console.log('Firing dataless route');
-      var ret = routes[object.route]();
-      res.end(ret);
-    }
-  }
-  catch(err){
-    res.end('Error Route not detected\n');
-  }
+    routing.Router(req, res, routes);
   });
 
 
---
+++ routing.js
@@ -0,0 +1,70 @@
+var RemoveSpaces = function(array){
+  for(var i = 0; i < array.length; i++){
+    if(array[i] == ""){
+      array.splice(i, 1);
+    }
+  }
+  return array;
+}
+
+var DetectRoute = function(routes, url){
+    console.log("Detecting Route for URL: " + url);
+    for(var key in routes){
+      routeParams = key.split('/');
+      routeParams = RemoveSpaces(routeParams);
+      for( var i = 0; i < routeParams.length; i++){
+        if(url.indexOf(routeParams[i]) != -1){
+          urlParams = url.split('/');
+	  urlParams = RemoveSpaces(urlParams);
+          if(urlParams.length == routeParams.length){
+            return { route : key, data : url };
+          }
+        }
+      }        
+    }
+
+}
+
+var RetrieveData = function(route, url){
+  if(route.indexOf(':') != -1){
+    data = new Object();
+    routePieces = route.split('/');
+    dataPieces = url.split('/');
+    for( var i=0; i < routePieces.length; i++){
+      if(routePieces[i].indexOf(':')  != -1){
+        data[routePieces[i].replace(/:/g, "")] = dataPieces[i];
+      }
+    }
+    return data;
+  }
+  else{
+    return;
+  }
+}
+
+var DetectAndDisplayRoute= function(req, res, routes){
+  try{
+    if(req.url == '/favicon.ico'){
+      return;
+    }
+    var object = DetectRoute(routes, req.url);
+    console.log("Matched route results: " + JSON.stringify(object));
+    var data = RetrieveData(object.route, object.data);
+    console.log("Route data pulled: " + JSON.stringify(data));
+    if(data != undefined){
+      console.log("firing data route");
+      var ret = routes[object.route](data);
+      res.end(ret);
+    }
+    else{
+      console.log('Firing dataless route');
+      var ret = routes[object.route]();
+      res.end(ret);
+    }
+  }
+  catch(err){
+    res.end('Error Route not detected\n');
+  }
+}
+
+exports.Router = DetectAndDisplayRoute;
